6e08f36d0203ad07e5ac9a1ad67c6f08e805569e
AlexCD <alexandru.cd@gmail.com>
2018-02-12 18:24:08 +0100

    Minor refactor
    - Removed method to splice the filename
    - Extract constructor code to multiple prototype methods
    - Switch from var to let and const
    - Add unit testing for getFileList and fileMap methods

--- index.js
+++ index.js
@@ -2,39 +2,40 @@
  * Created by AlexCD on 07/09/2015.
  */
 
-var fs = require('fs-extra');
-var path = require('path');
-var vow = require('vow');
-var fileExists = require('file-exists');
-var join = path.join;
+const fs = require('fs-extra');
+const path = require('path');
+const vow = require('vow');
+const fileExists = require('file-exists');
+const join = path.join;
 
 function WebpackClean (files, context) {
-  if (!Array.isArray(files)) files = [files];
-  //
-  this.files = files;
-  // get webpack root
-  this.context = context || path.dirname(module.parent.filename);
+  this.files = this.getFileList(files);
+  this.context = this.getContext(context); // get webpack root
 }
+
+WebpackClean.prototype.getFileList = function (files) {
+  return (!Array.isArray(files)) ? [files] : files;
+};
+
+WebpackClean.prototype.getContext = function (context) {
+  return context || path.dirname(module.parent.filename);
+};
 
 WebpackClean.prototype.filePath = function (file) {
   return join(this.context, file);
 };
 
-WebpackClean.prototype.fileSlice = function (file) {
-  // remove extension
-  return file.slice(0, -3);
-};
-
 WebpackClean.prototype.fileMap = function (file) {
-  var _self = this;
-  return _self.fileSlice(file) + '.js.map';
+  return file + '.map';
 };
 
 WebpackClean.prototype.cleanFile = function (file) {
-  var _defer = vow.defer();
-  var _removeFile = [
+  const _defer = vow.defer();
+  const _removeFile = [
     fs.remove(file, function (err) {
-      if (err) _defer.reject('WebpackClean: ' + err);
+      if (err) {
+        _defer.reject('WebpackClean: ' + err);
+      }
       console.log('File removed: ' + file);
     })
   ];
@@ -45,24 +46,27 @@ WebpackClean.prototype.cleanFile = function (file) {
 };
 
 WebpackClean.prototype.apply = function (compiler) {
-  var _self = this;
+  const _self = this;
 
   compiler.plugin('done', function (compilation) {
-    var _promises = [];
+    let _promises = [];
 
     _self.files.forEach(function (file) {
-      var _filePath = _self.filePath(file);
-      var _fileMap = _self.fileMap(_filePath);
+      const _filePath = _self.filePath(file);
+      const _fileMap = _self.fileMap(_filePath);
 
-      // remove file
-      _promises.push(_self.cleanFile(_filePath));
-
-      // automatically remove map files
-      if (fileExists(_fileMap)) _promises.push(_self.cleanFile(_fileMap));
+      // add to list the file to be removed
+      if (fileExists(_filePath)) {
+        _promises.push(_self.cleanFile(_filePath));
+      }
+      // add to list the map file to be removed
+      if (fileExists(_fileMap)) {
+        _promises.push(_self.cleanFile(_fileMap));
+      }
     });
 
     vow.all(_promises).then(function () {
-      // done
+      console.log('webpack-clean done')
     }).fail(function (text) {
       compilation.errors.push(new Error(text));
     });
--- test/test.js
+++ test/test.js
@@ -1,5 +1,15 @@
 import test from 'ava';
+const WebpackClean = require('../index');
 
-test('one plus one is two', t => {
-  t.is(1 + 1, 2);
+test('getFilesList should return a list of file/files', t => {
+  const getFileList = WebpackClean.prototype.getFileList;
+  t.deepEqual(getFileList('one.file.only'), ['one.file.only']);
+  t.deepEqual(getFileList(['file1.js', 'file2.js']), ['file1.js', 'file2.js']);
+  t.deepEqual(getFileList([]), []);
+});
+
+test('fileMap should return the map file name', t => {
+  const fileSlice = WebpackClean.prototype.fileMap;
+  t.is(fileSlice('filename'), 'filename.map');
+  t.is(fileSlice('file.name.js'), 'file.name.js.map');
 });
